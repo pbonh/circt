ce64fa7b9f842cc1600d7442a0033fb1e9b56240
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2022-02-28 15:21:26 -0800

    [OASIS] Initial commit for OASIS IR.

---
+++ lib/Dialect/OASIS/lib/Oasis/OasisOps.cpp
@@ -0,0 +1,14 @@
+//===- OasisOps.cpp - Oasis dialect ops ---------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "Oasis/OasisOps.h"
+#include "Oasis/OasisDialect.h"
+#include "mlir/IR/OpImplementation.h"
+
+#define GET_OP_CLASSES
+#include "Oasis/OasisOps.cpp.inc"
