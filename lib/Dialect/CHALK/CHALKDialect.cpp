d11010ab6bdb900e46f408e557424f8f645dd8e5
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2022-03-08 20:04:52 -0800

    [CHALK] Add rectangle op.

--- lib/Dialect/CHALK/CHALKDialect.cpp
+++ lib/Dialect/CHALK/CHALKDialect.cpp
@@ -32,32 +32,8 @@ void CHALKDialect::initialize() {
       >();
 }
 
-/// Registered hook to materialize a single constant operation from a given
-/// attribute value with the desired resultant type. This method should use
-/// the provided builder to create the operation without changing the
-/// insertion position. The generated operation is expected to be constant
-/// like, i.e. single result, zero operands, non side-effecting, etc. On
-/// success, this hook should return the value generated to represent the
-/// constant value. Otherwise, it should return null on failure.
-Operation *CHALKDialect::materializeConstant(OpBuilder &builder, Attribute value,
-                                            Type type, Location loc) {
-  // Integer constants.
-  if (auto intType = type.dyn_cast<IntegerType>())
-    if (auto attrValue = value.dyn_cast<IntegerAttr>())
-      return builder.create<hw::ConstantOp>(loc, type, attrValue);
-
-  // Parameter expressions materialize into hw.param.value.
-  auto parentOp = builder.getBlock()->getParentOp();
-  auto curModule = dyn_cast<hw::HWModuleOp>(parentOp);
-  if (!curModule)
-    curModule = parentOp->getParentOfType<hw::HWModuleOp>();
-  if (curModule && isValidParameterExpression(value, curModule))
-    return builder.create<hw::ParamValueOp>(loc, type, value);
-
-  return nullptr;
-}
-
 // Provide implementations for the enums we use.
 #include "circt/Dialect/CHALK/CHALKEnums.cpp.inc"
 
 #include "circt/Dialect/CHALK/CHALKDialect.cpp.inc"
+
