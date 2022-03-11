a35ae90887a391f1608ed6b654c7e7b66306b355
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2022-03-11 15:02:11 -0800

    [CHALK] Remove unused ops, add CellOp.

--- lib/Dialect/CHALK/CHALKOps.cpp
+++ lib/Dialect/CHALK/CHALKOps.cpp
@@ -18,10 +18,10 @@ using namespace circt;
 using namespace chalk;
 
 //===----------------------------------------------------------------------===//
-// MachineOp
+// CellOp
 //===----------------------------------------------------------------------===//
 
-void MachineOp::build(OpBuilder &builder, OperationState &state, StringRef name,
+void CellOp::build(OpBuilder &builder, OperationState &state, StringRef name,
                       Type stateType, FunctionType type,
                       ArrayRef<NamedAttribute> attrs,
                       ArrayRef<DictionaryAttr> argAttrs) {
@@ -39,11 +39,8 @@ void MachineOp::build(OpBuilder &builder, OperationState &state, StringRef name,
                                                 /*resultAttrs=*/llvm::None);
 }
 
-/// Get the default state of the machine.
-StateOp MachineOp::getDefaultState() { return *getOps<StateOp>().begin(); }
-
 /// Get the port information of the machine.
-void MachineOp::getHWPortInfo(SmallVectorImpl<hw::PortInfo> &ports) {
+void CellOp::getHWPortInfo(SmallVectorImpl<hw::PortInfo> &ports) {
   ports.clear();
   auto machineType = getType();
   auto builder = Builder(*this);
@@ -67,7 +64,7 @@ void MachineOp::getHWPortInfo(SmallVectorImpl<hw::PortInfo> &ports) {
   }
 }
 
-ParseResult MachineOp::parse(OpAsmParser &parser, OperationState &result) {
+ParseResult CellOp::parse(OpAsmParser &parser, OperationState &result) {
   auto buildFuncType =
       [](Builder &builder, ArrayRef<Type> argTypes, ArrayRef<Type> results,
          function_interface_impl::VariadicFlag,
@@ -77,7 +74,7 @@ ParseResult MachineOp::parse(OpAsmParser &parser, OperationState &result) {
       parser, result, /*allowVariadic=*/false, buildFuncType);
 }
 
-void MachineOp::print(OpAsmPrinter &p) {
+void CellOp::print(OpAsmPrinter &p) {
   FunctionType fnType = getType();
   function_interface_impl::printFunctionOp(
       p, *this, fnType.getInputs(), /*isVariadic=*/false, fnType.getResults());
@@ -97,7 +94,7 @@ static LogicalResult compareTypes(TypeRange rangeA, TypeRange rangeB) {
   return success();
 }
 
-static LogicalResult verifyMachineOp(MachineOp op) {
+static LogicalResult verifyCellOp(CellOp op) {
   // If this function is external there is nothing to do.
   if (op.isExternal())
     return success();
@@ -116,224 +113,6 @@ static LogicalResult verifyMachineOp(MachineOp op) {
   // Verify that the machine only has one block terminated with OutputOp.
   if (!llvm::hasSingleElement(op))
     return op.emitOpError("must only have a single block");
-
-  return success();
-}
-
-//===----------------------------------------------------------------------===//
-// InstanceOp
-//===----------------------------------------------------------------------===//
-
-/// Lookup the machine for the symbol.  This returns null on invalid IR.
-MachineOp InstanceOp::getMachine() {
-  auto module = (*this)->getParentOfType<ModuleOp>();
-  return module.lookupSymbol<MachineOp>(machine());
-}
-
-static LogicalResult verifyInstanceOp(InstanceOp op) {
-  auto machine = op.getMachine();
-  if (!machine)
-    return op.emitError("cannot find machine definition '")
-           << op.machine() << "'";
-
-  return success();
-}
-
-void InstanceOp::getAsmResultNames(
-    function_ref<void(Value, StringRef)> setNameFn) {
-  setNameFn(instance(), sym_name());
-}
-
-//===----------------------------------------------------------------------===//
-// TriggerOp
-//===----------------------------------------------------------------------===//
-
-template <typename OpType>
-static LogicalResult verifyCallerTypes(OpType op) {
-  auto machine = op.getMachine();
-  if (!machine)
-    return op.emitError("cannot find machine definition");
-
-  // Check operand types first.
-  if (failed(compareTypes(machine.getType().getInputs(),
-                          op.inputs().getTypes()))) {
-    auto diag =
-        op.emitOpError("operand types must match the machine input types");
-    diag.attachNote(machine->getLoc()) << "original machine declared here";
-    return failure();
-  }
-
-  // Check result types.
-  if (failed(compareTypes(machine.getType().getResults(),
-                          op.outputs().getTypes()))) {
-    auto diag =
-        op.emitOpError("result types must match the machine output types");
-    diag.attachNote(machine->getLoc()) << "original machine declared here";
-    return failure();
-  }
-
-  return success();
-}
-
-/// Lookup the machine for the symbol.  This returns null on invalid IR.
-MachineOp TriggerOp::getMachine() {
-  auto instanceOp = instance().getDefiningOp<InstanceOp>();
-  if (!instanceOp)
-    return nullptr;
-
-  return instanceOp.getMachine();
-}
-
-static LogicalResult verifyTriggerOp(TriggerOp op) {
-  return verifyCallerTypes(op);
-}
-
-//===----------------------------------------------------------------------===//
-// HWInstanceOp
-//===----------------------------------------------------------------------===//
-
-/// Lookup the machine for the symbol.  This returns null on invalid IR.
-MachineOp HWInstanceOp::getMachine() {
-  auto module = (*this)->getParentOfType<ModuleOp>();
-  return module.lookupSymbol<MachineOp>(machine());
-}
-
-static LogicalResult verifyHWInstanceOp(HWInstanceOp op) {
-  return verifyCallerTypes(op);
-}
-
-//===----------------------------------------------------------------------===//
-// StateOp
-//===----------------------------------------------------------------------===//
-
-LogicalResult StateOp::canonicalize(StateOp op, PatternRewriter &rewriter) {
-  bool hasAlwaysTakenTransition = false;
-  SmallVector<TransitionOp, 4> transitionsToErase;
-  // Remove all transitions after an "always-taken" transition.
-  for (auto transition : op.transitions().getOps<TransitionOp>()) {
-    if (!hasAlwaysTakenTransition)
-      hasAlwaysTakenTransition = transition.isAlwaysTaken();
-    else
-      transitionsToErase.push_back(transition);
-  }
-
-  for (auto transition : transitionsToErase)
-    rewriter.eraseOp(transition);
-
-  return failure(transitionsToErase.empty());
-}
-
-//===----------------------------------------------------------------------===//
-// OutputOp
-//===----------------------------------------------------------------------===//
-
-static LogicalResult verifyOutputOp(OutputOp op) {
-  if (op->getParentRegion() == &op->getParentOfType<StateOp>().transitions()) {
-    if (op.getNumOperands() != 0)
-      op.emitOpError("transitions region must not output any value");
-    return success();
-  }
-
-  // Verify that the result list of the machine and the operand list of the
-  // OutputOp line up.
-  auto machine = op->getParentOfType<MachineOp>();
-  if (failed(
-          compareTypes(machine.getType().getResults(), op.getOperandTypes())))
-    return op.emitOpError("operand types must match the machine output types");
-
-  return success();
-}
-
-//===----------------------------------------------------------------------===//
-// TransitionOp
-//===----------------------------------------------------------------------===//
-
-/// Lookup the next state for the symbol. This returns null on invalid IR.
-StateOp TransitionOp::getNextState() {
-  auto machineOp = (*this)->getParentOfType<MachineOp>();
-  if (!machineOp)
-    return nullptr;
-
-  return machineOp.lookupSymbol<StateOp>(nextState());
-}
-
-bool TransitionOp::isAlwaysTaken() {
-  auto guardReturn = getGuardReturn();
-  if (guardReturn.getNumOperands() == 0)
-    return true;
-
-  if (auto constantOp =
-          guardReturn.getOperand(0).getDefiningOp<mlir::arith::ConstantOp>())
-    return constantOp.getValue().cast<BoolAttr>().getValue();
-
-  return false;
-}
-
-LogicalResult TransitionOp::canonicalize(TransitionOp op,
-                                         PatternRewriter &rewriter) {
-  auto guardReturn = op.getGuardReturn();
-  if (guardReturn.getNumOperands() == 1)
-    if (auto constantOp = guardReturn.getOperand(0)
-                              .getDefiningOp<mlir::arith::ConstantOp>()) {
-      // Simplify when the guard region returns a constant value.
-      if (constantOp.getValue().cast<BoolAttr>().getValue()) {
-        // Replace the original return op with a new one without any operands
-        // if the constant is TRUE.
-        rewriter.setInsertionPoint(guardReturn);
-        rewriter.create<chalk::ReturnOp>(guardReturn.getLoc());
-        rewriter.eraseOp(guardReturn);
-      } else {
-        // Erase the whole transition op if the constant is FALSE, because the
-        // transition will never be taken.
-        rewriter.eraseOp(op);
-      }
-      return success();
-    }
-
-  return failure();
-}
-
-static LogicalResult verifyTransitionOp(TransitionOp op) {
-  if (!op.getNextState())
-    return op.emitOpError("cannot find the definition of the next state `")
-           << op.nextState() << "`";
-
-  // Verify the action region.
-  if (op.action().front().getTerminator()->getNumOperands() != 0)
-    return op.emitOpError("action region must not return any value");
-
-  // Verify the transition is located in the correct region.
-  if (op->getParentRegion() != &op.getCurrentState().transitions())
-    return op.emitOpError("must only be located in the transitions region");
-
-  return success();
-}
-
-//===----------------------------------------------------------------------===//
-// VariableOp
-//===----------------------------------------------------------------------===//
-
-void VariableOp::getAsmResultNames(
-    function_ref<void(Value, StringRef)> setNameFn) {
-  setNameFn(result(), name());
-}
-
-//===----------------------------------------------------------------------===//
-// UpdateOp
-//===----------------------------------------------------------------------===//
-
-/// Get the targeted variable operation. This returns null on invalid IR.
-VariableOp UpdateOp::getVariable() {
-  return variable().getDefiningOp<VariableOp>();
-}
-
-static LogicalResult verifyUpdateOp(UpdateOp op) {
-  if (!op.getVariable())
-    return op.emitOpError("destination is not a variable operation");
-
-  if (!op->getParentOfType<TransitionOp>().action().isAncestor(
-          op->getParentRegion()))
-    return op.emitOpError("must only be located in the action region");
 
   return success();
 }
