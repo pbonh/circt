2e1c8b68be95e592f00c5e71c43eac6b00d068b1
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2022-03-22 20:48:44 -0700

    [CHALK] Cleanup unused code, convert height/width to unsigned 32-bit integer.

--- lib/Dialect/CHALK/Transforms/InitPlacement.cpp
+++ lib/Dialect/CHALK/Transforms/InitPlacement.cpp
@@ -17,7 +17,7 @@
 #include "circt/Dialect/CHALK/CHALKTypes.h"
 #include "circt/Dialect/CHALK/Passes.h"
 #include "mlir/IR/Threading.h"
-#include "llvm/Support/Parallel.h"
+#include "llvm/Support/Casting.h"
 
 #include <set>
 
@@ -35,71 +35,31 @@ using RectangleOverlaps = std::set< RectangleOverlap >;
 namespace {
 struct InitPlacementPass : public InitPlacementBase<InitPlacementPass> {
     void runOnOperation() override;
-private:
-    RectangleList getRectangles();
-    bool checkOverlap(RectangleOp* rect1, RectangleOp* rect2);
-    RectangleOverlaps getOverlappingRectangles(const RectangleList& rectangles);
-    RectangleOverlap maxOverlap(const RectangleOverlaps& overlaps);
-    void fixOverlap(RectangleList& rectangles, const RectangleOverlap& maxOverlap);
 };
 } // end anonymous namespace
 
-RectangleList InitPlacementPass::getRectangles() {
+void InitPlacementPass::runOnOperation() {
     auto cell = getOperation();
     RectangleList rectangles;
     llvm::for_each(cell.getBody()->getOperations(), [&](Operation &op) {
         if (isa<RectangleOp>(op)) {
             auto rectangle = dyn_cast<RectangleOp>(op);
-            rectangles.push_back(&rectangle);
+            if (rectangle) {
+                rectangles.push_back(&rectangle);
+            }
         }
     });
-    return rectangles;
-}
 
-bool InitPlacementPass::checkOverlap(RectangleOp* rect1, RectangleOp* rect2) {
-    bool overlap{false};
-    struct RectDim {
-        int64_t x1, x2, y1, y2;
-    };
-    RectDim rect1Dim, rect2Dim;
-    rect1Dim.x1 = rect1->xCoord();
-    rect2Dim.x1 = rect2->xCoord();
-    rect1Dim.y1 = rect1->yCoord();
-    rect2Dim.y1 = rect2->yCoord();
-    rect1Dim.x2 = rect1Dim.x1 + rect1->width();
-    rect2Dim.x2 = rect2Dim.x1 + rect2->width();
-    rect1Dim.y2 = rect1Dim.y1 + rect1->height();
-    rect2Dim.y2 = rect2Dim.y1 + rect2->height();
-    overlap = (rect1Dim.x1 < rect2Dim.x2) && (rect1Dim.x2 > rect2Dim.x1) && (rect1Dim.y1 > rect2Dim.y2) && (rect1Dim.y2 < rect2Dim.y1);
-    return overlap;
-}
-
-RectangleOverlaps InitPlacementPass::getOverlappingRectangles(const RectangleList& rectangles) {
-    RectangleOverlaps overlaps;
-    for (auto *rect: rectangles) {
-    }
-    return overlaps;
-}
-
-RectangleOverlap InitPlacementPass::maxOverlap(const RectangleOverlaps& overlaps) {
-    RectangleOverlap overlap{0, 0};
-    return overlap;
-}
-
-void InitPlacementPass::fixOverlap(RectangleList& rectangles, const RectangleOverlap& maxOverlap) {
-}
-
-void InitPlacementPass::runOnOperation() {
-    auto rectangles = getRectangles();
     size_t idx = 0;
-    int64_t prevX = 0;
+    int64_t placeX = 0;
     for (auto *rect: rectangles) {
-        IntegerAttr prevXAttr;
-        // auto prevXAttr = IntegerAttr::get(&getContext(), prevX);
+        uint64_t prevX = rect->xCoord();
+        IntegerAttr placeXAttr;
+        // auto placeXAttr = IntegerAttr::get(&getContext(), placeX);
         if (idx != 0) {
-            rect->xCoordAttr(prevXAttr);
+            rect->xCoordAttr(placeXAttr);
         }
-        prevX = rect->xCoord();
+        placeX += prevX;
     }
 }
 
