6ee8664ebdb343d2508fcd459c98536be8439b10
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2022-04-29 16:49:42 -0700

    [CHALK] More boilerplate for iterating a FIRRTL circuit/module.

--- lib/Conversion/FIRRTLToCHALK/FIRRTLToCHALK.cpp
+++ lib/Conversion/FIRRTLToCHALK/FIRRTLToCHALK.cpp
@@ -15,10 +15,13 @@
 #include "../PassDetail.h"
 #include "circt/Dialect/CHALK/CHALKOps.h"
 #include "circt/Dialect/FIRRTL/FIRRTLOps.h"
+#include "circt/Dialect/FIRRTL/FIRRTLVisitors.h"
 #include "circt/Dialect/HW/HWOps.h"
 #include "mlir/Dialect/ControlFlow/IR/ControlFlowOps.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/BuiltinDialect.h"
+#include "mlir/IR/ImplicitLocOpBuilder.h"
+#include "mlir/IR/Threading.h"
 #include "mlir/Transforms/DialectConversion.h"
 
 using namespace mlir;
@@ -49,33 +52,39 @@ struct AndPrimOpConversion : public OpConversionPattern<AndPrimOp> {
 } // namespace
 
 //===----------------------------------------------------------------------===//
-// Conversion Infrastructure
-//===----------------------------------------------------------------------===//
-
-static void populateLegality(ConversionTarget &target) {
-  target.addLegalDialect<mlir::BuiltinDialect>();
-  target.addLegalDialect<hw::HWDialect>();
-  target.addLegalDialect<firrtl::FIRRTLDialect>();
-  target.addLegalDialect<chalk::CHALKDialect>();
-}
-
-static void populateTypeConversion(TypeConverter &typeConverter) {}
-
-static void populateOpConversion(RewritePatternSet &patterns,
-                                 TypeConverter &typeConverter) {
-  auto *context = patterns.getContext();
-  // clang-format off
-  patterns.add<
-    AndPrimOpConversion
-  >(typeConverter, context);
-  // clang-format on
-  mlir::populateFunctionOpInterfaceTypeConversionPattern<func::FuncOp>(
-      patterns, typeConverter);
-}
-
-//===----------------------------------------------------------------------===//
 // FIRRTL to CHALK Conversion Pass
 //===----------------------------------------------------------------------===//
+
+namespace {
+struct FIRRTLCHALKEmbed;
+
+/// This is state shared across the parallel module embedding logic.
+struct CircuitEmbeddingState {
+};
+
+struct FIRRTLCHALKEmbed : public FIRRTLVisitor<FIRRTLCHALKEmbed, LogicalResult> {
+  FIRRTLCHALKEmbed(firrtl::FModuleOp module, CircuitEmbeddingState &circuitState)
+      : circuitState(circuitState),
+        builder(module.getLoc(), module.getContext()) {}
+
+  LogicalResult run();
+
+private:
+  FModuleOp firrtlModule;
+  CircuitEmbeddingState &circuitState;
+  ImplicitLocOpBuilder builder;
+};
+} // namespace
+
+LogicalResult FIRRTLCHALKEmbed::run() {
+  for (auto &moduleOps : firrtlModule) {
+  }
+  // OperationState state(firrtlModule.getLoc(), firrtlModule->getName());
+  // Region *region = state.addRegion();
+  // Block *body = new Block();
+  // region->push_back(body);
+  return success();
+}
 
 namespace {
 struct FIRRTLToCHALKPass : public FIRRTLToCHALKBase<FIRRTLToCHALKPass> {
@@ -83,7 +92,7 @@ struct FIRRTLToCHALKPass : public FIRRTLToCHALKBase<FIRRTLToCHALKPass> {
 };
 } // namespace
 
-/// Create a FIRRTL to core dialects conversion pass.
+/// Create a FIRRTL to CHALK conversion(embedding) pass.
 std::unique_ptr<OperationPass<ModuleOp>>
 circt::createConvertFIRRTLToCHALKPass() {
   return std::make_unique<FIRRTLToCHALKPass>();
@@ -91,16 +100,37 @@ circt::createConvertFIRRTLToCHALKPass() {
 
 /// This is the main entrypoint for the FIRRTL to CHALK conversion pass.
 void FIRRTLToCHALKPass::runOnOperation() {
-  MLIRContext &context = getContext();
-  ModuleOp module = getOperation();
+  auto *topLevelModule = getOperation().getBody();
 
-  ConversionTarget target(context);
-  TypeConverter typeConverter;
-  RewritePatternSet patterns(&context);
-  populateLegality(target);
-  populateTypeConversion(typeConverter);
-  populateOpConversion(patterns, typeConverter);
+  // Find the single firrtl.circuit in the module.
+  CircuitOp circuit;
+  for (auto &op : *topLevelModule) {
+    if ((circuit = dyn_cast<CircuitOp>(&op)))
+      break;
+  }
 
-  if (failed(applyFullConversion(module, target, std::move(patterns))))
-    signalPassFailure();
+  if (!circuit)
+    return;
+
+  auto *circuitBody = circuit.getBody();
+  SmallVector<FModuleOp, 32> modulesToProcess;
+  CircuitEmbeddingState state;
+
+  for (auto &op : make_early_inc_range(circuitBody->getOperations())) {
+    TypeSwitch<Operation *>(&op)
+        .Case<FModuleOp>([&](auto module) {
+          modulesToProcess.push_back(module);
+        })
+        .Default([&](Operation *op) {
+        });
+  }
+
+  auto result = mlir::failableParallelForEachN(
+      &getContext(), 0, modulesToProcess.size(), [&](auto index) {
+        return FIRRTLCHALKEmbed(modulesToProcess[index], state).run();
+      });
+
+  if (failed(result))
+    return signalPassFailure();
+
 }
